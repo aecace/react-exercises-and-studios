8624b0e8229f3948271bde62a93469d57a918d1d
Amanda Crawford <crawforda0502@gmail.com>
2024-03-21 20:38:02 -0500

    React exercises pt 2 done

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,27 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+function Profile() {
+   const listItem = oceans.map(oceans =>
+      <div className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`} key={oceans.id}>
+         <h1>{oceans.name}</h1>
+         <img className='img' src={oceans.image} alt={oceans.name} />
+         <h2>Fun Facts:</h2>
+         <ul>
+            <li>{oceans.fact1}</li>
+            <li>{oceans.fact2}</li>
+            <li>{oceans.fact3}</li>
+         </ul>
          <Button />
-      </>  
+      </div>
+   );
+
+   return(
+      <ul>
+         {listItem}
+      </ul>
    );
 }
 
